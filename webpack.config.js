6220ad3b2ce9a20786e355605671f900a63c1920
Vinicius G. Martin <viniciusgulartemartin@gmail.com>
2018-11-27 19:04:37 -0200

    add react presets

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
           loader: "babel-loader"
@@ -10,4 +10,4 @@ module.exports = {
       }
     ]
   }
-}
+};
